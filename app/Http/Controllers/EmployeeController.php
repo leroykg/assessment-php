203c395bd606aa69960982a0c4263024e3d1f62c
leroykg <leroykg@gmail.com>
2021-11-24 10:06:36 +0200

    Create the routes and ink them to empty controller methods for now

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+use App\Models\EmployeeSkills;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Show all employees
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Create a new employee
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display a specified employee.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Edit an employee
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified employee.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified employee.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Get all employees
+Route::get('employees', [EmployeeController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Get a single employee
+Route::get('employees/{employee}', [EmployeeController::class, 'show']);
+
+//Create a new employee
+Route::post('employees', [EmployeeController::class, 'create']);
+
+//Update an employee
+Route::put('employees/{id}', [EmployeeController::class, 'update']);
+
+//Delete an employee
+Route::delete('employees/{employee}', [EmployeeController::class, 'destroy']);
